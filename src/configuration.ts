fed5423b1ee5abbcb247c7b99063c7f1ebd8e7a6
yourname <your@email.org>
2019-06-10 15:07:38 +0800

    allow using postfix like _test.ts as extension

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,44 +1,43 @@
 import * as vscode from 'vscode';
 
 export interface Association {
-    extension: string;
-    associated: {
-        [key: string]: string[] | undefined
-    };
+  extension: string;
+  associated: {[key: string]: string[]|undefined};
 }
 
 export function getAssociationFor(fileName: string) {
-    return getConfiguration()
-        .find(a => fileName.endsWith(a.extension));
+  return getConfiguration().find(a => fileName.endsWith(a.extension));
 }
 
 function getConfiguration() {
-    return sanitizeConfiguration(
-        vscode.workspace.getConfiguration()
-            .get('switcher.associations'));
+  return sanitizeConfiguration(
+      vscode.workspace.getConfiguration().get('switcher.associations'));
 }
 
 function sanitizeConfiguration(configuration: any): Association[] {
-    return configuration && Array.isArray(configuration)
-        ? configuration.filter(a => isAssociation(a))
-        : [];
+  return configuration && Array.isArray(configuration) ?
+      configuration.filter(a => isAssociation(a)) :
+      [];
 }
 
 function isAssociation(value: any) {
-    return !!(value
-        && (value as Association).extension && isExtension((value as Association).extension)
-        && (value as Association).associated && isExtensionArrayDict((value as Association).associated));
+  return !!(
+      value && (value as Association).extension &&
+      isExtension((value as Association).extension) &&
+      (value as Association).associated &&
+      isExtensionArrayDict((value as Association).associated));
 }
 
 function isExtensionArrayDict(value: any) {
-    return Object.keys(value)
-        .every(k => Array.isArray(value[k]) && (value[k] as string[]).every(isExtension));
+  return Object.keys(value).every(
+      k =>
+          Array.isArray(value[k]) && (value[k] as string[]).every(isExtension));
 }
 
 function isExtension(value: any) {
-    if (typeof value !== 'string') {
-        return false;
-    }
+  if (typeof value !== 'string') {
+    return false;
+  }
 
-    return /^\.[\w.]+$/.test(value);
+  return /^(_\w+)?\.[\w.]+$/.test(value);
 }
